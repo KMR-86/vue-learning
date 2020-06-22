c8f0f444b30722c9e25b20a17786abf64b8af509
khandaker mushfiqur rahman <1505086.kmr@ugrad.cse.buet.ac.bd>
2020-06-22 20:01:11 +0600

    navbar done

---
+++ vue-homepage/src/App.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <div id="app">
+    <navbar/>
+  </div>
+</template>
+
+<script>
+
+import navbar from './components/navbar.vue'
+//import navbar2 from './components/navbar-2.vue'
+export default {
+  name: 'App',
+  components: {
+    navbar
+  }
+}
+</script>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0;
+}
+
+
+
+</style>
+
